f95afe356b4b107b8c4c6ee512c94fa1a0ff04c5
Carine Batista <carinedcb@hotmail.com>
2021-03-11 19:21:20 -0300

    Add card.js and organize package.json

---
+++ card.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+// Modules
+const boxen = require("boxen");
+const chalk = require("chalk");
+const clear = require("clear");
+const sw = require("star-wars-quotes");
+clear();
+
+const data = {
+    name: chalk.red("                  Carine Batista"),
+    work: `${chalk.white("Front End Developer at")} ${chalk.cyan("Braavo")}`,
+
+    github: chalk.gray("https://github.com/") + chalk.blue("carinebatista"),
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyanBright("carinebatata"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.green("carinebatista"),
+    web: chalk.cyan("https://carine.dev"),
+
+    npx: chalk.red("npx carine"),
+
+    labelWork: ("         "),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        
+        `${chalk.italic("    My inbox is always open, Whether you have a")}`,
+        `${chalk.italic(
+            "    question or just want talk about Star Wars! "
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "bold",
+        borderColor: "magenta"
+    }
+);
+
+console.log(me);
+
+console.log(`" ${chalk.cyanBright.italic(sw())} "`);
